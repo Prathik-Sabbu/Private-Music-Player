e4dbc00c6cd8087d937cc419def7eae56b2033b2
Prathik-Sabbu <115114850+Prathik-Sabbu@users.noreply.github.com>
2026-03-20 08:29:21 -0400

    updates

---
+++ public/songs.js
@@ -0,0 +1,7 @@
+const songs = [
+    {id: 1, title: "Song name", artist: "Artist 1", album: "Album 1", year: 2020, genre: "Genre 1", duration: 210, src: "https://example.com/song1.mp3", cover: "https://example.com/cover1.jpg", playCount:0},
+    {id: 2, title: "Another song", artist: "Artist 2", album: "Album 2", year: 2021, genre: "Genre 2", duration: 180, src: "https://example.com/song2.mp3", cover: "https://example.com/cover2.jpg", playCount:0},
+    {id: 3, title: "Third song", artist: "Artist 3", album: "Album 3", year: 2019, genre: "Genre 3", duration: 240, src: "https://example.com/song3.mp3", cover: "https://example.com/cover3.jpg", playCount:0},
+]
+
+module.exports = songs;

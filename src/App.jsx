9a3688b246e6fe54415a3d77d1fd06507ea69ad4
Prathik-Sabbu <115114850+Prathik-Sabbu@users.noreply.github.com>
2026-04-13 01:24:26 -0400

    added app.jsx and main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import usePlayerStore from './store/playerStore';
+
+// Page Imports
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import LibraryPage from './pages/LibraryPage';
+import PlaylistHub from './pages/PlaylistPage';
+import PlaylistDetail from './pages/PlaylistDetail';
+
+// Component Imports
+import PlayerBar from './components/PlayerBar';
+import BottomNav from './components/BottomNav';
+import NowPlayingSheet from './components/NowPlayingSheet';
+
+function App() {
+    const initAudio = usePlayerStore((state) => state.initAudio);
+
+    useEffect(() => {
+        initAudio();
+    }, [initAudio]);
+
+    return (
+        <div className="flex flex-col h-screen bg-black text-white overflow-hidden">
+        <main className="flex-1 overflow-y-auto pb-32"> 
+            <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/library" element={<LibraryPage />} />
+            <Route path="/playlists" element={<PlaylistPage />} />
+            <Route path="/playlist/:id" element={<PlaylistDetail />} />
+            </Routes>
+        </main>
+
+        <div className="fixed bottom-0 left-0 right-0 z-50">
+            <NowPlayingSheet /> 
+            <PlayerBar />
+            <BottomNav />
+        </div>
+        </div>
+    );
+}
+
+export default App;

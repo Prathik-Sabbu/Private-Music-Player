1ae24cb54c8d9c1391987dc57df450c087eff946
Prathik-Sabbu <115114850+Prathik-Sabbu@users.noreply.github.com>
2026-05-14 00:04:43 -0400

    Updated Layout,jsx to replace App.jsx job

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { usePlayerStore } from './store/playerStore';
+import Layout from './components/Layout';
 
 // Page Imports
 import HomePage from './pages/HomePage';
@@ -9,27 +9,19 @@ import LibraryPage from './pages/LibraryPage';
 import PlaylistPage from './pages/PlaylistPage';
 import PlaylistDetail from './pages/PlaylistDetail';
 
-// Component Imports
-import BottomNav from './components/BottomNav';
 
 function App() {
 
     return (
-        <div className="flex flex-col h-screen bg-black text-white overflow-hidden">
-        <main className="flex-1 overflow-y-auto pb-32"> 
+        <Layout>
             <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/library" element={<LibraryPage />} />
-            <Route path="/playlists" element={<PlaylistPage />} />
-            <Route path="/playlist/:id" element={<PlaylistDetail />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/search" element={<SearchPage />} />
+                <Route path="/library" element={<LibraryPage />} />
+                <Route path="/playlists" element={<PlaylistPage />} />
+                <Route path="/playlist/:id" element={<PlaylistDetail />} />
             </Routes>
-        </main>
-
-        <div className="fixed bottom-0 left-0 right-0 z-50 w-full bg-[#030303] border-t border-white/10">
-            <BottomNav />
-        </div>
-        </div>
+        </Layout>
     );
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BottomNav from './BottomNav.jsx';
+
+const Layout = ({ children }) => {
+
+    return (
+        <div className="flex flex-col h-screen bg-black text-white overflow-hidden">
+        <main className="flex-1 overflow-y-auto pb-32"> 
+            {children}
+        </main>
+
+        <div className="fixed bottom-0 left-0 right-0 z-50 w-full bg-[#030303] border-t border-white/10">
+            <BottomNav />
+        </div>
+        </div>
+    );
+};
+
+export default Layout;
